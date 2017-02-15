74678f89863e57c3612423be355ca1d9c5607dc4
tuengThanawut <t.nunthanee@gmail.com>
2017-02-15 15:22:02 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="register.php">
+		<table>
+			<tr>
+				<td>
+					<label>ชื่อผู้ใช้</label>
+				</td>
+				<td>
+					<input type="text" name="username">
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
